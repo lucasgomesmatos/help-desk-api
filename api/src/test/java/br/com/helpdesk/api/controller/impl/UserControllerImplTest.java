b798cb8b1f55378ddad6eaff69d30c39c5f0c54a
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2025-01-01 18:53:25 -0300

    feat: add test update user

--- api/src/test/java/br/com/helpdesk/api/controller/impl/UserControllerImplTest.java
+++ api/src/test/java/br/com/helpdesk/api/controller/impl/UserControllerImplTest.java
@@ -3,6 +3,7 @@ package br.com.helpdesk.api.controller.impl;
 import br.com.helpdesk.api.entity.User;
 import br.com.helpdesk.api.repository.UserRepository;
 import br.com.helpdesk.commons.models.requests.CreateUserRequest;
+import br.com.helpdesk.commons.models.requests.UpdateUserRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -18,8 +19,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static br.com.helpdesk.api.creator.CreatorUtils.generateMock;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -90,8 +90,8 @@ class UserControllerImplTest {
                 .withEmail(validEmail);
 
         mockMvc.perform(post("/api/users")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(toJson(request)))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(toJson(request)))
                 .andExpect(status().isCreated());
 
         repository.delete(repository.findByEmail(validEmail).get());
@@ -109,8 +109,8 @@ class UserControllerImplTest {
                 .withEmail(entity.getEmail());
 
         mockMvc.perform(post("/api/users")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(toJson(request)))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(toJson(request)))
                 .andExpect(status().isConflict())
                 .andExpect(jsonPath("$.message").value("Email [ " + entity.getEmail() + " ] already exists"))
                 .andExpect(jsonPath("$.status").value(HttpStatus.CONFLICT.value()))
@@ -122,10 +122,160 @@ class UserControllerImplTest {
         repository.delete(entity);
     }
 
+    @Test
+    void saveUserWithNameNullThenBadRequest() throws Exception {
+        final var validEmail = "teste@mail.com";
+        final var request = generateMock(CreateUserRequest.class)
+                .withEmail(validEmail)
+                .withName("");
+
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(toJson(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Exception in validation atributtes"))
+                .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.error").value("Validation Exception"))
+                .andExpect(jsonPath("$.path").value("/api/users"))
+                .andExpect(jsonPath("$.errors[?(@.fieldName == 'name' && @.message == 'Name must contain between 3 and 50 characters')]").exists())
+        ;
+    }
+
+    @Test
+    void saveUserWithEmailNullThenBadRequest() throws Exception {
+        final var request = generateMock(CreateUserRequest.class)
+                .withEmail("");
+
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(toJson(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Exception in validation atributtes"))
+                .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.error").value("Validation Exception"))
+                .andExpect(jsonPath("$.path").value("/api/users"))
+                .andExpect(jsonPath("$.errors[?(@.fieldName == 'email' && @.message == 'Email is required')]").exists())
+        ;
+    }
+
+    @Test
+    void saveUserWithInvalidEmailThenBadRequest() throws Exception {
+        final var request = generateMock(CreateUserRequest.class)
+                .withEmail("invalid");
+
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(toJson(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Exception in validation atributtes"))
+                .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.error").value("Validation Exception"))
+                .andExpect(jsonPath("$.path").value("/api/users"))
+                .andExpect(jsonPath("$.errors[?(@.fieldName == 'email' && @.message == 'Invalid email')]").exists())
+        ;
+    }
+
+    @Test
+    void saveUserWithPasswordNullThenBadRequest() throws Exception {
+        final var validEmail = "teste@mail.com";
+        final var request = generateMock(CreateUserRequest.class)
+                .withEmail(validEmail)
+                .withPassword("");
+
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(toJson(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Exception in validation atributtes"))
+                .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.error").value("Validation Exception"))
+                .andExpect(jsonPath("$.path").value("/api/users"))
+                .andExpect(jsonPath("$.errors[?(@.fieldName == 'password' " +
+                        "&& @.message == 'Password must contain between 6 and 50 characters')]").exists())
+        ;
+    }
+
+    @Test
+    void updateWithSuccess() throws Exception {
+        final var validEmail = UUID.randomUUID() + "@mail.com";
+        final var entity = generateMock(User.class)
+                .withEmail(validEmail);
+        final var userId = repository.save(entity).getId();
+
+        final var request = generateMock(CreateUserRequest.class)
+                .withEmail(validEmail);
+
+        mockMvc.perform(put("/api/users/{id}", userId)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(toJson(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(userId))
+                .andExpect(jsonPath("$.name").value(request.name()))
+                .andExpect(jsonPath("$.email").value(request.email()))
+                .andExpect(jsonPath("$.profiles").isArray())
+        ;
+
+        repository.delete(entity);
+    }
+
+    @Test
+    void updateWithConflict() throws Exception {
+        final var validEmail = "testeUpdate@mail.com";
+
+        final var entity1 = generateMock(User.class)
+                .withEmail(validEmail);
+
+        repository.save(entity1);
+
+        final var entity2 = generateMock(User.class)
+                .withEmail(UUID.randomUUID() + "@mail.com");
+
+        final var userId = repository.save(entity2).getId();
+
+
+        final var request = generateMock(UpdateUserRequest.class)
+                .withEmail(validEmail);
+
+        mockMvc.perform(put("/api/users/{id}", userId)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(toJson(request)))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message").value("Email [ " + validEmail + " ] already exists"))
+                .andExpect(jsonPath("$.status").value(HttpStatus.CONFLICT.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.error").value(HttpStatus.CONFLICT.getReasonPhrase()))
+                .andExpect(jsonPath("$.path").value("/api/users/" + userId))
+        ;
+
+        repository.delete(entity1);
+        repository.delete(entity2);
+    }
+
+    @Test
+    void updateWithNotFound() throws Exception {
+        final var validEmail = UUID.randomUUID() + "@mail.com";
+        final var request = generateMock(UpdateUserRequest.class)
+                .withEmail(validEmail);
+
+        mockMvc.perform(put("/api/users/{id}", "1")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(toJson(request)))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Object not found 1, Type: User"))
+                .andExpect(jsonPath("$.status").value(HttpStatus.NOT_FOUND.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.error").value(HttpStatus.NOT_FOUND.getReasonPhrase()))
+                .andExpect(jsonPath("$.path").value("/api/users/1"))
+        ;
+    }
+
 
     private static String toJson(Object object) throws JsonProcessingException {
         return new ObjectMapper().writeValueAsString(object);
     }
-
 
 }
