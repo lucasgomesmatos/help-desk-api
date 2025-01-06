f528a2aa8298b9c0b5c30c362adb54af3ce8b4bc
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2025-01-05 21:06:28 -0300

    feat: add config server

--- user-service-api/api/src/main/java/br/com/helpdesk/api/Application.java
+++ user-service-api/api/src/main/java/br/com/helpdesk/api/Application.java
@@ -3,6 +3,7 @@ package br.com.helpdesk.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
