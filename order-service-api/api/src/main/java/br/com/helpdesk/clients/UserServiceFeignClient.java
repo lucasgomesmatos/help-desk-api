af2318581c0c3165bb18a3312eba41e5a74106c3
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2025-01-10 20:02:44 -0300

    feat: add connection with service discovery

--- order-service-api/api/src/main/java/br/com/helpdesk/clients/UserServiceFeignClient.java
+++ order-service-api/api/src/main/java/br/com/helpdesk/clients/UserServiceFeignClient.java
@@ -6,7 +6,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service-api", url = "${user.service.uri}")
+@FeignClient(
+        name = "user-service-api",
+        path = "/api/users"
+)
 public interface UserServiceFeignClient {
 
     @GetMapping("/{id}")
