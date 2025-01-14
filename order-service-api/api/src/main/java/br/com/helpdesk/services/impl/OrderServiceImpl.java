346dc3737516555b2b1571979517ef33c9c5a66d
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2025-01-14 07:45:54 -0300

    chore: add rabbitmq

--- order-service-api/api/src/main/java/br/com/helpdesk/services/impl/OrderServiceImpl.java
+++ order-service-api/api/src/main/java/br/com/helpdesk/services/impl/OrderServiceImpl.java
@@ -7,12 +7,14 @@ import br.com.helpdesk.repositories.OrderRepository;
 import br.com.helpdesk.services.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import models.dtos.OrderCreatedMessage;
 import models.enums.OrderStatusEnum;
 import models.exceptions.ResourceNotFoundException;
 import models.requests.CreateOrderRequest;
 import models.requests.OrderResponse;
 import models.requests.UpdateOrderRequest;
 import models.responses.UserResponse;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -29,6 +31,7 @@ public class OrderServiceImpl implements OrderService {
     private final OrderRepository repository;
     private final OrderMapper mapper;
     private final UserServiceFeignClient userServiceFeignClient;
+    private final RabbitTemplate rabbitTemplate;
 
     @Override
     public OrderResponse findById(final Long id) {
@@ -37,14 +40,24 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public void save(CreateOrderRequest request) {
-        final var requesterId = validateUser(request.requesterId());
-        final var customerId = validateUser(request.customerId());
-        log.info("Requester: {}", requesterId);
-        log.info("Customer: {}", customerId);
+        final var requester = validateUser(request.requesterId());
+        final var customer = validateUser(request.customerId());
 
-        repository.save(
+        var entity = repository.save(
                 mapper.fromRequest(request)
         );
+
+        rabbitTemplate.convertAndSend(
+                "helpdesk",
+                "rk.orders.create",
+                new OrderCreatedMessage(
+                        mapper.fromEntity(entity),
+                        requester,
+                        customer
+                )
+        );
+
+
     }
 
     @Override
